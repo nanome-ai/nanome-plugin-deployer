d9969a8a8f0e887b50db5577234791e074c3cafd
Mike Rosengrant <mjrosengrant@users.noreply.github.com>
2022-03-08 17:41:50 -0500

    Deploy from images (#4)
    * Add deploy_from_images.shto deploy using ECR images
    
    * Refactor get_plugin_args, sucessfully deploy vault
    
    * Set up docker-compose to launch starter-stack
    
    Co-authored-by: Alex McNerney <ajm13@me.com>

---
+++ deploy_from_images.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# Use Public ECR images to deploy plugins stack
+source base.sh
+
+echo "./deploy_from_images.sh $*" > redeploy.sh
+
+REGISTRY_URI="public.ecr.aws/h7r1e4h2"
+REPO_NAMES=(
+    "chemical-preview"
+    "chemical-interactions"
+    "chemical-properties"
+    "coordinate-align"
+    # "docking-autodock4"
+    "docking-smina"
+    "esp"
+    "hydrogens"
+    "minimization"
+    "realtime-scoring"
+    "rmsd"
+    "structure-prep"
+    "vault"
+    "vault-server"
+)
+
+TAG="latest"
+
+echo -e "\npulling plugin images..."
+for REPO in "${REPO_NAMES[@]}"; do (
+    IMAGE_URI="$REGISTRY_URI/$REPO:$TAG"
+    echo -n "$REPO... "
+    docker pull $IMAGE_URI >/dev/null
+    docker tag $IMAGE_URI $REPO
+    echo "done"
+); done
+
+echo -e "\ndeploying plugins..."
+cd $INSTALL_DIRECTORY
+for plugin_name in "${plugins[@]}"; do (
+    echo -n "$plugin_name... "
+    if [ -d "$plugin_name" ]; then
+        rm -rf $plugin_name/*
+    else
+        mkdir -p $plugin_name
+    fi
+
+    cd $plugin_name
+    wget -q https://raw.githubusercontent.com/nanome-ai/plugin-$plugin_name/master/docker/deploy.sh
+    chmod +x deploy.sh
+
+    get_plugin_index $plugin_name
+    arg_string="${args[@]} ${plugin_args[$plugin_index]}"
+    read -ra args <<< "$arg_string"
+
+    ./deploy.sh "${args[@]}" 1>> "$logs/$plugin_name.log"
+    echo "done"
+); done
+
+echo -e "\ndone"
